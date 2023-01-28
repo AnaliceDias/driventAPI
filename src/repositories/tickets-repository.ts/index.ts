5efa4e32222a254da7c2752b40b49cf8c6baafd3
Analice <anadias.alice@gmail.com>
2023-01-28 11:06:52 -0300

    feat: findTicketByUserId repository

--- src/repositories/tickets-repository.ts/index.ts
+++ src/repositories/tickets-repository.ts/index.ts
@@ -4,8 +4,22 @@ async function findTicketsTypes() {
   return prisma.ticketType.findMany();
 }
 
+async function findTicketByUserId(userId: number) {
+  return prisma.ticket.findFirst({
+    where: {
+      Enrollment: {
+        userId,
+      },
+    },
+    include: {
+      TicketType: true,
+    },
+  });
+}
+
 const ticketsRepository = {
   findTicketsTypes,
+  findTicketByUserId,
 };
 
 export default ticketsRepository;
