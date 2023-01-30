e9d87f5571b786155c79dca14ffcae7c63cb7d79
Analice <anadias.alice@gmail.com>
2023-01-29 22:57:05 -0300

    feat: createTicket controller

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,4 +1,5 @@
 import { AuthenticatedRequest } from "@/middlewares";
+import enrollmentsService from "@/services/enrollments-service";
 import ticketsService from "@/services/tickets-sevice";
 import { Response } from "express";
 import httpStatus from "http-status";
@@ -17,6 +18,23 @@ export async function getTicketByUserId(req: AuthenticatedRequest, res: Response
   try{
     const tickets = await ticketsService.getTicketByUserId(userId);
     return res.send(tickets).status(httpStatus.OK);
+  } catch (error) {
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
+}
+
+export async function createTicket(req: AuthenticatedRequest, res: Response) {
+  const { ticketTypeId } = req.body;
+  const { userId } = req;
+
+  if (!ticketTypeId) res.sendStatus(httpStatus.BAD_REQUEST);
+
+  try{
+    const enrollment = await enrollmentsService.getOneWithAddressByUserId(userId);
+    const enrollmentId = enrollment.id;
+    const newTicket = await ticketsService.createTicket(ticketTypeId, enrollmentId);
+
+    return res.status(httpStatus.CREATED).send(newTicket);
   } catch (error) {
     return res.sendStatus(httpStatus.NOT_FOUND);
   }
