cf6fba9626e0743a0063f97ee698c837f0e90c78
Analice <anadias.alice@gmail.com>
2023-01-30 11:20:22 -0300

    feat: checkUserId service

--- src/services/payments-service/index.ts
+++ src/services/payments-service/index.ts
@@ -1,5 +1,6 @@
-import { notFoundError } from "@/errors"; 
+import { notFoundError, unauthorizedError } from "@/errors"; 
 import paymentsRepository from "@/repositories/payments-repository";
+import { Enrollment } from "@prisma/client";
 
 async function getPaymentsByTicketId(ticketId: number) {
   const payment = await paymentsRepository.findPaymentsByTicketId(ticketId);
@@ -8,7 +9,12 @@ async function getPaymentsByTicketId(ticketId: number) {
   return payment;
 }
 
+async function checkUserId(enrollment: Enrollment, userId: number) {
+  if(enrollment.userId !== userId) throw unauthorizedError();
+  return;
+}
 const paymentsServices = {
+  checkUserId,
   getPaymentsByTicketId,
 };
   
