d3b08e7c25a3046bd52d499bd66a73fd7162c5ab
Analice <anadias.alice@gmail.com>
2023-01-29 22:39:50 -0300

    feat: +rota get /

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { authenticateToken } from "@/middlewares";
-import { getTicketsTypes } from "@/controllers/tickets-controller";
+import { getTicketByUserId, getTicketsTypes } from "@/controllers/tickets-controller";
 
 const ticketsRouter = Router();
 
 ticketsRouter
   .all("/*", authenticateToken)
-  .get("/types", getTicketsTypes);
+  .get("/types", getTicketsTypes)
+  .get("/", getTicketByUserId);
 
 export { ticketsRouter };
