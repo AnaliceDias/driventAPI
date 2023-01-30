4ef389f7bd7fc7c242697614d5a47e3460062e72
Analice <anadias.alice@gmail.com>
2023-01-30 11:25:13 -0300

    feat: processPayment controller

--- src/controllers/payments-controller.ts
+++ src/controllers/payments-controller.ts
@@ -1,7 +1,9 @@
 import { AuthenticatedRequest } from "@/middlewares";
 import paymentsServices from "@/services/payments-service";
+import ticketsService from "@/services/tickets-sevice";
 import { Response } from "express";
 import httpStatus from "http-status";
+import { stringify } from "querystring";
 
 export async function getPaymentsByTicketId(req: AuthenticatedRequest, res: Response) {
   const { ticketId } = req.query;
@@ -10,6 +12,31 @@ export async function getPaymentsByTicketId(req: AuthenticatedRequest, res: Resp
     const payment = await paymentsServices.getPaymentsByTicketId(+ticketId);
     
     return res.status(httpStatus.OK).send(payment);
+  } catch (error) {
+    if (error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
+    if (error.name === "UnauthorizedError") return res.sendStatus(httpStatus.UNAUTHORIZED);
+  }
+}
+
+export async function processPayment(req: AuthenticatedRequest, res: Response) {
+  const { ticketId } = req.body;
+  const ticket = res.locals.ticket;
+  let cardLastDigits = req.body.cardData.number;
+  cardLastDigits = cardLastDigits.substring(cardLastDigits.lenght - 4);
+
+  try {
+    const price = await ticketsService.getTicketPrice(ticket.ticketTypeId);
+
+    const paymentData = {
+      ticketId: +ticketId,
+      value: price,
+      cardIssuer: stringify(req.body.cardData.issuer),
+      cardLastDigits: stringify(cardLastDigits),
+    };
+
+    const newPayment = await paymentsServices.registerPayment(paymentData);
+    
+    return res.status(httpStatus.OK).send(newPayment);
   } catch (error) {
     if (error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
     if (error.name === "UnauthorizedError") return res.sendStatus(httpStatus.UNAUTHORIZED);
