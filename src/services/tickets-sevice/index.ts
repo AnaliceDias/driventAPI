5efb852ea579a8a586ae00c2914f7c009c418936
Analice <anadias.alice@gmail.com>
2023-01-29 22:43:26 -0300

    feat: getTicketByUserId service

--- src/services/tickets-sevice/index.ts
+++ src/services/tickets-sevice/index.ts
@@ -7,8 +7,15 @@ async function getTicketsTypes() {
   return result;
 }
 
+async function getTicketByUserId(userId: number) {
+  const result = await ticketsRepository.findTicketByUserId(userId);
+  if (!result) throw notFoundError();
+  return result;
+}
+
 const ticketsService = {
   getTicketsTypes,
+  getTicketByUserId
 };
 
 export default ticketsService;
