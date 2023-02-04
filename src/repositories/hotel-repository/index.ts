18891719443f58a6aaaa380c590b9d42fa9306c9
Analice <anadias.alice@gmail.com>
2023-02-04 16:41:37 -0300

    feat: findHotelRoomsById repository

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -4,8 +4,25 @@ async function findHotels() {
   return prisma.hotel.findMany();
 }
 
+async function findHotelRoomsById(hotelId: number) {
+  const hotel = await prisma.hotel.findUnique({
+    where: {
+      id: hotelId
+    }
+  });
+
+  const rooms = await prisma.room.findMany({
+    where: {
+      hotelId: hotelId,
+    },
+  });
+
+  return { ...hotel, Rooms: [...rooms] };
+}
+
 const hotelsRepository = {
   findHotels,
+  findHotelRoomsById,
 };
     
 export default hotelsRepository;
