648507c21662eb993472719cbaf76ce38f1ba9e6
Analice <anadias.alice@gmail.com>
2023-02-04 16:37:47 -0300

    feat: getHotelRoomsById controller

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -12,3 +12,16 @@ export async function getHotels(req: AuthenticatedRequest, res: Response) {
     return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
   }
 }
+
+export async function getHotelRoomsById(req: AuthenticatedRequest, res: Response) {
+  const hotelId = +req.params.hotelId;
+
+  if(!hotelId) res.sendStatus(httpStatus.BAD_REQUEST);
+
+  try {
+    const rooms = await hotelsService.getHotelRoomsById(hotelId);
+    return res.status(httpStatus.OK).send(rooms);
+  } catch (error) {
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
+}
