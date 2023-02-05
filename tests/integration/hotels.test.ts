ce2f9a4a63a17e78764a49ee15f69ef8e117d0ae
Analice <anadias.alice@gmail.com>
2023-02-05 01:01:41 -0300

    test: should respond with status 200 and the list of hotels

--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -4,7 +4,10 @@ import supertest from "supertest";
 import { cleanDb, generateValidToken } from "../helpers";
 import { TicketStatus } from "@prisma/client";
 import * as jwt from "jsonwebtoken";
-import { createUser } from "../factories";
+import { createEnrollmentWithAddress, createPayment, createTicket, createTicketTypeCustomizable, createUser } from "../factories";
+import { createHotels } from "../factories/hotels-factory";
+
+const server = supertest(app);
 
 beforeAll(async () => {
   await init();
@@ -35,4 +38,28 @@ describe("Authentication tests for the hotels route", () => {
   });
 });
 
-const server = supertest(app);
+describe("GET /hotels", () => {
+  it("should respond with status 200 and the list of hotels", async () => {
+    const user = await createUser();
+    const token = await generateValidToken(user);
+    const enrollment = await createEnrollmentWithAddress(user);
+    const ticketType = await createTicketTypeCustomizable(false, true);
+    const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+    await createPayment(ticket.id, ticketType.price);
+    await createHotels();
+
+    const response = await server.get("/hotels").set("Authorization", `Bearer ${token}`);
+    
+    expect(response.status).toEqual(httpStatus.OK);
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ 
+          id: expect.any(Number),
+          name: expect.any(String),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String) 
+        })
+      ])
+    );
+  });
+});
