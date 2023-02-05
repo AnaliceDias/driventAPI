75f06c5aad808b42f791ecbe0c88b786883cbc0f
Analice <anadias.alice@gmail.com>
2023-02-05 09:06:01 -0300

    refactor: +checkEnrollment middleware

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { authenticateToken,  } from "@/middlewares";
 import hotelsMiddleware from "@/middlewares/hotels-middleware";
 import { getHotelRoomsById, getHotels } from "@/controllers/hotels-controller";
+import enrollmentssMiddleware from "@/middlewares/enrollments-middleware";
 
 const hotelsRouter = Router();
 
 hotelsRouter
-  .all("/*", authenticateToken, hotelsMiddleware.checkIncludesHotel)
+  .all("/*", authenticateToken, enrollmentssMiddleware.checkEnrollment, hotelsMiddleware.checkIncludesHotel)
   .get("/", getHotels)
   .get("/:hotelId", getHotelRoomsById);
 
