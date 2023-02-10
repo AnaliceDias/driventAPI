20230fe2fb63465424ce4fa1b14da69684153644
Analice <anadias.alice@gmail.com>
2023-02-09 22:59:56 -0300

    feat: +rota put /:bookingId

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authenticateToken, checkVacancies } from "@/middlewares";
-import { getBookingByToken, registerBooking } from "@/controllers/booking-controller";
+import { getBookingByToken, registerBooking, updateBooking } from "@/controllers/booking-controller";
 
 const bookingRouter = Router();
 
 bookingRouter
   .all("/*", authenticateToken)
   .get("/", getBookingByToken)
-  .post("/", checkVacancies, registerBooking);
+  .post("/", checkVacancies, registerBooking)
+  .put("/:bookingId", checkVacancies, updateBooking);
 
 export { bookingRouter };
