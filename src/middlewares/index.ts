c80c8308f63a441960eaa70514f4d0f677d415d9
Analice <anadias.alice@gmail.com>
2023-02-09 22:42:09 -0300

    feat: export booking-middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,3 +2,4 @@ export * from "./error-handling-middleware";
 export * from "./validation-middleware";
 export * from "./authentication-middleware";
 export * from "./hotels-middleware";
+export * from "./booking-middleware";
