e991c1d1abbf53572e616a8676e89640e33a0bdd
Analice <anadias.alice@gmail.com>
2023-02-10 22:21:30 -0300

    test: should respond with status 401 if given token is not valid

---
+++ tests/integration/booking.test.ts
@@ -0,0 +1,39 @@
+import app, { init } from "@/app";
+import { prisma } from "@/config";
+import faker from "@faker-js/faker";
+import { TicketStatus } from "@prisma/client";
+import httpStatus from "http-status";
+import * as jwt from "jsonwebtoken";
+import supertest from "supertest";
+import { createEnrollmentWithAddress, createHotel, createPayment, createRoomWithHotelId, createTicket, createTicketTypeWithHotel, createUser, generateNonExistentRoomId } from "../factories";
+import { createBooking } from "../factories/booking-factory";
+import { cleanDb, generateValidToken } from "../helpers";
+
+beforeAll(async () => {
+  await init();
+});
+
+beforeEach(async () => {
+  await cleanDb();
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
+});
+
+const server = supertest(app);
+
+describe("/booking when there are authentication problems", () => {
+  it("should respond with status 401 if given token is not valid", async () => {
+    const token = faker.lorem.word();
+    const id = faker.random.numeric();
+
+    const responsePost = await server.post("/booking").set("Authorization", `Bearer ${token}`);
+    const responseGet = await server.get("/booking").set("Authorization", `Bearer ${token}`);
+    const responsePut = await server.put(`/booking/${id}`).set("Authorization", `Bearer ${token}`);
+
+    expect(responsePost.status).toBe(httpStatus.UNAUTHORIZED);
+    expect(responseGet.status).toBe(httpStatus.UNAUTHORIZED);
+    expect(responsePut.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+});
