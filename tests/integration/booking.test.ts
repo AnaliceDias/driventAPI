d88271285dda9822b6ac24b6dbd10937cb05c023
Analice <anadias.alice@gmail.com>
2023-02-10 22:22:56 -0300

    test: should respond with status 401 if there is no session for given token

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -50,4 +50,18 @@ describe("/booking when there are authentication problems", () => {
     expect(responseGet.status).toBe(httpStatus.UNAUTHORIZED);
     expect(responsePut.status).toBe(httpStatus.UNAUTHORIZED);
   });
+
+  it("should respond with status 401 if there is no session for given token", async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+    const id = faker.random.numeric();
+
+    const responsePost = await server.post("/booking").set("Authorization", `Bearer ${token}`);
+    const responseGet = await server.post("/booking").set("Authorization", `Bearer ${token}`);
+    const responsePut = await server.post(`/booking/${id}`).set("Authorization", `Bearer ${token}`);
+
+    expect(responsePost.status).toBe(httpStatus.UNAUTHORIZED);
+    expect(responseGet.status).toBe(httpStatus.UNAUTHORIZED);
+    expect(responsePut.status).toBe(httpStatus.UNAUTHORIZED);
+  });
 });
